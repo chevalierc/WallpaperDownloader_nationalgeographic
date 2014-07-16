404c94dda921ab09e8299901a8d56fac3bec4fc7
dakshinai <akdaks08@gmail.com>
2014-07-16 05:42:45 -0500

    first commit

---
+++ WallpaperDownloader_nationalgeographic/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using HtmlAgilityPack;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WallpaperDownloader_nationalgeographic
+{
+    class Program
+    {
+        const string app_identifier = "WallpaperDownloader_nationalgeographic";
+
+        static void Main(string[] args)
+        {
+
+            #region Html Structure
+
+            // Load HTML source into a string
+
+            //   <div class="primary_photo">
+
+            //    <a title="Go to the previous Photo of the Day" href="/photography/photo-of-the-day/elephant-pair-kadur/">
+
+
+            //<img width="990" height="742" alt="Picture of a porcelain crab and an anemone" src="http://images.nationalgeographic.com/wpf/media-live/photos/000/692/cache/porcelain-crab-anemone_69224_990x742.jpg">	
+
+
+            //    </a>
+            //</div><!-- .primary_photo-->
+
+            // Bing Image Result for Cat, First Page
+
+            #endregion
+
+            try
+            {
+                string url = System.Configuration.ConfigurationManager.AppSettings["URL"];
+
+                // For speed of dev, I use a WebClient
+                WebClient client = new WebClient();
+                string html = client.DownloadString(url);
+
+                // Load the Html into the agility pack
+                HtmlDocument doc = new HtmlDocument();
+
+                doc.LoadHtml(html);
+
+                // Now, using LINQ to get all Images
+                List<HtmlNode> imageNodes = null;
+                imageNodes = (from HtmlNode node in doc.DocumentNode.SelectNodes("//img")
+                              where node.Name == "img"
+                              && node.Attributes["src"] != null
+                              && node.Attributes["src"].Value.StartsWith("http://images.nationalgeographic.com")
+                              select node).ToList();
+
+
+                if (imageNodes.Count > 0)
+                {
+                    string tempPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\temp_nationalgeographic";
+                    client.DownloadFile(imageNodes[0].Attributes["src"].Value, tempPath);
+
+                    string style = System.Configuration.ConfigurationManager.AppSettings["STYLE"];
+
+                    switch (style)
+                    {
+                        case "FILLED":
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Filled);
+                            break;
+                        case "CENTERED":
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Centered);
+                            break;
+                        case "FITTED":
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Fitted);
+                            break;
+                        case "STRETCHED":
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Stretched);
+                            break;
+                        case "TILED":
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Tiled);
+                            break;
+                        default:
+                            Wallpaper.Set(tempPath, Wallpaper.Style.Fitted);
+                            break;
+                    }
+
+
+                }
+                else
+                {
+                    System.Diagnostics.EventLog.WriteEntry(app_identifier, "No Identifiable Image Structure. Unable to idenify the image URL", System.Diagnostics.EventLogEntryType.Warning);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                if (!System.Diagnostics.EventLog.SourceExists(app_identifier))
+                    System.Diagnostics.EventLog.CreateEventSource(app_identifier, "Application");
+                System.Diagnostics.EventLog.WriteEntry(app_identifier, e.Message, System.Diagnostics.EventLogEntryType.Warning);
+            }
+
+
+        }
+    }
+}
---
+++ WallpaperDownloader_nationalgeographic/Wallpaper.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace WallpaperDownloader_nationalgeographic
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched,
+            Filled,
+            Fitted
+        }
+        static void GetImageFormat(System.Drawing.Image img, out System.Drawing.Imaging.ImageFormat imgFormat)
+        {
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Bmp))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Bmp;
+
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Emf))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Emf;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Exif))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Exif;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Gif))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Gif;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Icon))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Icon;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.MemoryBmp))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.MemoryBmp;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Png))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Png;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Tiff))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Tiff;
+            }
+            if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Wmf))
+            {
+                imgFormat = System.Drawing.Imaging.ImageFormat.Wmf;
+            }
+
+
+            imgFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+
+        }
+
+        static string GetFilenameExtension(System.Drawing.Imaging.ImageFormat format)
+        {
+            return System.Drawing.Imaging.ImageCodecInfo.GetImageEncoders()
+                                 .First(x => x.FormatID == format.Guid)
+                                 .FilenameExtension
+                                 .Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                                 .First()
+                                 .Trim('*');
+        }
+        public static void Set(string wpaper, Style style)
+        {
+            System.Drawing.Image img = System.Drawing.Image.FromFile(wpaper);
+
+            System.Drawing.Imaging.ImageFormat imageFormat;
+            GetImageFormat(img, out imageFormat);
+            string ext = GetFilenameExtension(imageFormat);
+
+            img.Save(wpaper + ext);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 0.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 0.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+            if (style == Style.Filled)
+            {
+                key.SetValue(@"WallpaperStyle", 10.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+            if (style == Style.Fitted)
+            {
+                key.SetValue(@"WallpaperStyle", 6.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                wpaper,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
+
